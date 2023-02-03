dfad015d17cc90b90241e8231163c06b8215e383
vedant <modivedant39@gmail.com>
2023-02-03 15:02:30 +0530

    Add : Eureka server

--- E-Commerce-Service/src/main/java/com/ecommerce/demo/EcommerceDemoApplication.java
+++ E-Commerce-Service/src/main/java/com/ecommerce/demo/EcommerceDemoApplication.java
@@ -2,8 +2,10 @@ package com.ecommerce.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+
 public class EcommerceDemoApplication {
 
 	public static void main(String[] args) {
