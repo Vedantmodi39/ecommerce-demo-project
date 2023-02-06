3bf37a60b4a4b7f6c1ea9c89ccff8a1d75cba94d
nisarg <nisargvyas7130@gmail.com>
2023-02-06 15:03:44 +0530

    added Dto.

---
+++ E-Commerce-Service/src/main/java/com/ecommerce/demo/dto/UserDto.java
@@ -0,0 +1,16 @@
+package com.ecommerce.demo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDto {
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String phoneNo;
+}
